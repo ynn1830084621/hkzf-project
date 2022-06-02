8bfdddee52e9cdadaf618a37f9da016852ed8092
ynn1830084621 <1830084621@qq.com>
2022-06-02 23:07:32 +0800

    "导航栏"

--- hkzf-mobile/src/pages/Index/index.js
+++ hkzf-mobile/src/pages/Index/index.js
@@ -1,45 +1,69 @@
 import React, { useState, useEffect } from 'react';
-import { Carousel } from 'antd';
+import { Carousel, Breadcrumb } from 'antd';
+import { HomeOutlined, TeamOutlined, EnvironmentOutlined, KeyOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
 const contentStyle = {
-    dispaly: 'inline-block',
-    width: '100%', 
-    height: 212
+  dispaly: 'inline-block',
+  width: '100%', 
+  height: 212
 };
 
 function Index() {
-    const [swipers, getSwipers] = useState([])
-    //获取轮播图数据
-    useEffect(() => {
-        const fetchDate = async () => {
-            const res = await axios.get('http://localhost:8080/home/swiper').then(
-                (res) => {
-                    return res.data.body
-                }    
-            )
-            //console.log('获取轮播图数据', res)
-            getSwipers(res)
-        }
-        fetchDate()
-    },[])
-    return(
+  const [swipers, getSwipers] = useState([])
+  //获取轮播图数据
+  useEffect(() => {
+    const fetchDate = async () => {
+      const res = await axios.get('http://localhost:8080/home/swiper').then(
+        (res) => {
+          return res.data.body
+        }    
+      )
+      //console.log('获取轮播图数据', res)
+      getSwipers(res)
+    }
+    fetchDate()
+  },[])
+  return(
+    <div>
+      <div>
         <Carousel autoplay>
-            { swipers.map( item => {
-                //console.log('item',item);
-                return (
-                    <a key={item.id} href='http://itcat.cn' style={contentStyle}>
-                        <img 
-                            src={`http://localhost:8080${item.imgSrc}`} 
-                            alt=''
-                            style={{width: '100%', verticalAlign: 'top'}}
-                        />
-                    </a>
-                )
-            })}
-
+          { swipers.map( item => {
+            //console.log('item',item);
+            return (
+              <a key={item.id} href='http://itcat.cn' style={contentStyle}>
+                <img 
+                  src={`http://localhost:8080${item.imgSrc}`} 
+                  alt=''
+                  style={{width: '100%', verticalAlign: 'top'}}
+                />
+              </a>
+            )
+          })}
         </Carousel>
-    )
+      </div>
+      <div>
+        <Breadcrumb>
+            <Breadcrumb.Item>
+              <HomeOutlined />
+              <p>整租</p>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <TeamOutlined />
+              <p>合租</p>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <EnvironmentOutlined />
+              <p>地图找房</p>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <KeyOutlined />
+              <p>出租</p>
+            </Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+    </div>
+  )
 
 };
 
